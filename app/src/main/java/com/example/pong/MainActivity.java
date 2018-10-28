8bfd0f8fbc8dc75717866c3a93f1032b2b5099ce
Tomáš Lamacz <tlamacz@seznam.cz>
2018-10-28 11:04:18 +0100

    vytvoreni hlavniho menu

--- app/src/main/java/com/example/pong/MainActivity.java
+++ app/src/main/java/com/example/pong/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pong;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
@@ -9,5 +10,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onePlayerOnClick(android.view.View view){
+        Intent onePlayerActivity = new Intent(getBaseContext(), com.example.pong.OnePlayerActivity.class);
+        startActivity(onePlayerActivity);
+    }
+
+    public void twoPlayersOnClick(android.view.View view){
+        Intent twoPlayersActivity = new Intent(getBaseContext(), com.example.pong.TwoPlayersActivity.class);
+        startActivity(twoPlayersActivity);
+    }
+
+    public void WallOnePlayerOnClick(android.view.View view){
+        Intent wallOnePlayerActivity = new Intent(getBaseContext(), com.example.pong.WallOnePlayerActivity.class);
+        startActivity(wallOnePlayerActivity);
     }
 }
